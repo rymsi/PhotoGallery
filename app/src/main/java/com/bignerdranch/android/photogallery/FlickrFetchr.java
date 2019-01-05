59ee072b25b2fc08180f3317f2746f452104c87f
junaidjag@gmail.com <junaidjag@gmail.com>
2019-01-04 21:18:54 -0500

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.java
@@ -0,0 +1,20 @@
+package com.bignerdranch.android.photogallery;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class FlickrFetchr {
+    public byte[] getUrlBytes(String urlSpec) throws IOException {
+        URL url = new URL(urlSpec);
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+
+        }
+    }
+}
---
+++ app/src/main/java/com/bignerdranch/android/photogallery/SingleFragmentActivity.java
@@ -0,0 +1,35 @@
+package com.bignerdranch.android.photogallery;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment(); //forces an implementation of this in any class that extends this abstract class
+
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getSupportFragmentManager(); //just gets the fragment manager which handles all the fragments for this activity
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container); //either obtains fragment from saved state, or null if first time
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+
+    }
+
+}
