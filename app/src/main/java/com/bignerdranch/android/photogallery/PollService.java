60333372f4788134f6c984d99d511e04448a6c35
junaidjag@gmail.com <junaidjag@gmail.com>
2019-01-09 23:05:21 -0500

    Added a service, and the ability to search Flickr

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
@@ -0,0 +1,6 @@
+package com.bignerdranch.android.photogallery;
+
+import android.app.IntentService;
+
+public class PollService extends IntentService {
+}
---
+++ app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.java
@@ -0,0 +1,4 @@
+package com.bignerdranch.android.photogallery;
+
+public class ThumbnailDownloader {
+}
